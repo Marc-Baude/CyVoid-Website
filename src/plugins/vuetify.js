b53b4bf5b7a0cefadf8404e471056824c02ca48e
Marc Baude <marcbaude@gmail.com>
2023-01-12 16:24:02 +0100

    Init vuetify & animate.style

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,31 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+const myTheme = {
+    dark: true,
+    colors:{
+        primary: '#85009A',
+        secondary: '#7D0077',
+        accent: '#DB0DFF',
+        error: '#b71c1c',
+        background: '#440015',
+        surface: '#1e1e1e',
+        info: '#E53BFF',
+        success: '#4caf50',
+        warning: '#ffeb3b',
+        textlight: '#ffffff',
+        textdark: '#000000',
+    }
+}
+export default createVuetify({
+    theme: {
+        defaultTheme: 'myTheme',
+        themes: {
+            myTheme,
+        }
+    }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-btn
+        icon="mdi-heart"
+        color="primary"
+    ></v-btn>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+  },
+});
+</script>
